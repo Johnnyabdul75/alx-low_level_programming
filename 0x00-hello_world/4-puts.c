7e82eb1072f4cc1525fff62400071e73312ffa65
johnnyabdul75 <johnnyabdul75@gmail.com>
2022-09-08 16:26:30 -0700

    sweet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #!/bin/bash
-#include <stdio.h>
+ #include <stdio.h>
 /**
  *  * main - main block
  *   * Return: 0
@@ -7,5 +7,4 @@
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+
