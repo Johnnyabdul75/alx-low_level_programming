ab5f9a4d165992529454532f3c57aba6e5e87bb0
johnnyabdul75 <johnnyabdul75@gmail.com>
2022-09-10 01:17:25 -0700

    sweet home

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#incude <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description -Write a program that prints decimal numbera
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+for (n - 0; n < 10; n++)
+{
+printf("%d", n);
+}
+{
+printf("\n");
+}
+return (0);
+}
