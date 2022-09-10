35a52122ad8ac5dc9c8a41131dfaaa15394dc494
johnnyabdul75 <johnnyabdul75@gmail.com>
2022-09-10 15:17:21 -0700

    comme home

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
-/**
-*main - Entry point
-*
-*Description -Write a program that prints decimal numbera
-*
-*Return: Always 0 (success)
-*/
-int main(void)
-{
-int n;
-for (n - 0; n < 10; n++)
-{
-printf("%d", n);
-}
-{
-printf("\n");
-}
-return (0);
+#include <stdio.h> 
+
+/** 
+*  main - print if the number is positive, zero, or negative
+*  
+*  Description: using the main function
+*  this program prints "Programming is positive, zero, or negative 
+*  Return: 0 
+*/                                         
+int main(void)
+{ 
+int i; 
+for (i = 0; i < 10; i++)
+{ 
+printf("%i", i); 
+}                                                                     
+printf("\n"); 
+return (0); 
 }
