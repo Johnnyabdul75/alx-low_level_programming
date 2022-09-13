7225925779f45b275327d4f1cfc039853c624e6a
johnnyabdul75 <johnnyabdul75@gmail.com>
2022-09-13 14:32:53 -0700

    home cell

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_N_INCLUDED
+#define HOLBERTON_N_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_10(void);
+int _islower(int c);
+int _isalpa(int c);
+int print_sign(int n);
+int labs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_times_table(void);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+#endif
+
