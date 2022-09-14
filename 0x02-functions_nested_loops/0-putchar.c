794dc9f3097b5b808e625526b80a65cb0dddeec3
johnnyabdul75 <johnnyabdul75@gmail.com>
2022-09-14 00:48:02 -0700

    home coming

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,24 @@
-#include "main.h"
-
+#include "main. h"
 /**
-* main - prints putchar
-* Return: always 0
+**main - Entry point
+*
+*@char : description, it's a character type
+*@_putchar() : is a function
+*Return: Always 0 (success)
+*
 */
+
+char _putchar(char);
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char *ch = "_putchar",
+while (*ch)
+{
+_putchar(*ch);
+ch++;
+}
+{
 _putchar('\n');
+}
 return (0);
 }
