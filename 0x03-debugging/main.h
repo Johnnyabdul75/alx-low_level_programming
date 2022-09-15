f4c375cbf573090754c927883bf4921ebb3d210a
johnnyabdul75 <johnnyabdul75@gmail.com>
2022-09-15 11:22:22 -0700

    sweer home

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void positive_or_negative(int, i);
+int largest_numver(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_days(int, int);
