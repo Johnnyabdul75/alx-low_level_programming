a9e785b665fc348db4bb62fe7ebeab6633c13510
johnnyabdul75 <johnnyabdul75@gmail.com>
2022-09-19 10:13:35 -0700

    sweet home

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include <main.h>
-
 /**
- * reset_to_98 - updates the value of points n to 98
- *
- * @n: input
- */
+ *  * reset_to_98 - updates the value of points n to 98
+ *   *
+ *    * @n: input
+ *     */
 void reset_to_98(int *n)
 {
-	*n = 98;
+	        *n = 98;
 }
