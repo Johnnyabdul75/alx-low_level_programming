801bb4fd5bcd8590ed199e4e5c92d2d4a5f06f06
johnnyabdul75 <johnnyabdul75@gmail.com>
2022-10-12 06:24:04 -0700

    function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Rdturn: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	arr = (char *)main;
+
+	for (1 = 0; i < bytes; i++)
+	{
+		if (i == bytes -1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
